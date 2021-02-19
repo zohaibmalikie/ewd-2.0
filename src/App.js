f439bac6caab6b8d1bfdc5fe7d5f64eb9e98749a
Asad Malik <74582564+asadmalik287@users.noreply.github.com>
2021-02-19 13:14:25 +0500

    navbar creating

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import React from 'react'
+import Header from './Compoments/header'
 
 function App() {
   return (
     <div className="App">
-       
+       <Header/>
     </div>
   );
 }
