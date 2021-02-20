aa29d6ccd7b269103ea18d560ad866fe59a37762
Asad Malik <74582564+asadmalik287@users.noreply.github.com>
2021-02-20 15:33:24 +0500

    porfolio

--- src/Compoments/cards.js
+++ src/Compoments/cards.js
@@ -6,139 +6,34 @@ import thirdImg from './assets/img/DESIGN_SERVICES.png'
 
 function cards() {
   return (
-    <div className="main_padding">
-      <div className="row">
-        <div className="col-12 col-sm-6 col-md-4 col-lg-4">
-          <div className="our-team">
-            <div className="picture">
-              <img
-                className="img-fluid p-4 w-100 h-100"
-                src={firstImg}
-              />
-            </div>
-            <div className="team-content">
-              <h3 className="name">E-Commerce Solutions</h3>
-            </div>
-            <ul className="social">
-              <li>
-                <a>
-                We provide extensive E commerce solution along with 24/7 help and support in any regard
-                </a>
-              </li>
-              {/* <li>
-                <a
-                  href="https://codepen.io/collection/XdWJOQ/"
-                  className="fa fa-twitter"
-                  aria-hidden="true"
-                ></a>
-              </li>
-              <li>
-                <a
-                  href="https://codepen.io/collection/XdWJOQ/"
-                  className="fa fa-google-plus"
-                  aria-hidden="true"
-                ></a>
-              </li>
-              <li>
-                <a
-                  href="https://codepen.io/collection/XdWJOQ/"
-                  className="fa fa-linkedin"
-                  aria-hidden="true"
-                ></a>
-              </li> */}
-            </ul>
-          </div>
-        </div>
-        <div className="col-12 col-sm-6 col-md-4 col-lg-4">
-          <div className="our-team">
-            <div className="picture">
-              <img
-                className="img-fluid p-4"
-                src={secondImg}
-              />
-            </div>
-            <div className="team-content">
-              <h3 className="name">Justin Ramos</h3>
-              <h4 className="title">Web Developer</h4>
-            </div>
-            <ul className="social">
-              <li>
-                <a
-                  href="https://codepen.io/collection/XdWJOQ/"
-                  className="fa fa-facebook"
-                  aria-hidden="true"
-                ></a>
-              </li>
-              <li>
-                <a
-                  href="https://codepen.io/collection/XdWJOQ/"
-                  className="fa fa-twitter"
-                  aria-hidden="true"
-                ></a>
-              </li>
-              <li>
-                <a
-                  href="https://codepen.io/collection/XdWJOQ/"
-                  className="fa fa-google-plus"
-                  aria-hidden="true"
-                ></a>
-              </li>
-              <li>
-                <a
-                  href="https://codepen.io/collection/XdWJOQ/"
-                  className="fa fa-linkedin"
-                  aria-hidden="true"
-                ></a>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className="col-12 col-sm-6 col-md-4 col-lg-4">
-          <div className="our-team">
-            <div className="picture">
-              <img
-                className="img-fluid p-4"
-                src={thirdImg}
-              />
-            </div>
-            <div className="team-content">
-              <h3 className="name">Mary Huntley</h3>
-              <h4 className="title">Web Developer</h4>
-            </div>
-            <ul className="social">
-              <li>
-                <a
-                  href="https://codepen.io/collection/XdWJOQ/"
-                  className="fa fa-facebook"
-                  aria-hidden="true"
-                ></a>
-              </li>
-              <li>
-                <a
-                  href="https://codepen.io/collection/XdWJOQ/"
-                  className="fa fa-twitter"
-                  aria-hidden="true"
-                ></a>
-              </li>
-              <li>
-                <a
-                  href="https://codepen.io/collection/XdWJOQ/"
-                  className="fa fa-google-plus"
-                  aria-hidden="true"
-                ></a>
-              </li>
-              <li>
-                <a
-                  href="https://codepen.io/collection/XdWJOQ/"
-                  className="fa fa-linkedin"
-                  aria-hidden="true"
-                ></a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
+ <section className="main_padding">
+   <div className="row">
+
+     <div className="col-lg-4 col-md-4"><div class=" card" >
+                    <div className="card-head circle-inner" id="card-header"></div>
+                    <div className="card-body mt-1">
+                        <div className="circle aos-init aos-animate">
+                            <img src="{firstImg}" className="img-fluid" id="solution"/>
+
+                        </div>
+
+                    </div>
+                    <div className=" card-footer h-180" style="border:0px;">
+                        <h3 className="heading-card"> E-Commerce Solutions </h3>
+                        <p className="three-card-only">We provide extensive E commerce solution along with 24/7 help and
+                            support in any regard </p>
+                    </div>
+
+
+
+
+                </div></div>
+     <div className="col-lg-4 col-md-4"></div>
+     <div className="col-lg-4 col-md-4"></div>
+   </div>
+
+
+ </section>
   );
 }
 
