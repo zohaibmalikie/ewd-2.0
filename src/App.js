580d630bb7adeed20eff6a3f222e38008b28dda5
zohaibmalikie <zohaibmalikie@gmail.com>
2021-02-20 13:14:54 +0500

    video & cards layout added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import React from 'react'
 import Header from './Compoments/header'
 import Footer from './Compoments/footer'
+import Card from './Compoments/cards'
 
 function App() {
   return (
     <div className="App">
        <Header/>
+       {/* <Card/> */}
        <Footer/>
     </div>
   );
